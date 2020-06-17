bfb4c217fdfa40532507459473e094b7f9c95c6a
Sheelu27 <sheelukumari27@gmail.com>
2020-06-17 23:51:49 +0530

    Layout Updated

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 var about = document.getElementById("option1")
 var contact = document.getElementById("option3")
+var home = document.getElementById("option4")
+var login = document.getElementById("option2")
 
 
 function aboutit(){
@@ -13,5 +15,18 @@ function contactit(){
 
 }
 
+function homein(){
+    event.preventDefault()
+    location.href = "index.html"
+}
+
+function signin(){
+    event.preventDefault()
+    location.href = "login.html"
+}
+
 about.addEventListener('click',aboutit)
 contact.addEventListener('click',contactit)
+home.addEventListener('click',homein)
+login.addEventListener('click',signin)
+
